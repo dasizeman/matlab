7b2fef5e579db935e415b962f73c6766776510ce
Dave Sizer <dave@sizetron.net>
2016-09-29 15:15:03 -0400

    Finished proj 1 problem 1

--- bisect.m
+++ bisect.m
@@ -1,4 +1,4 @@
-function bisect(f, a, b, N, TOL)
+function res=bisect(f, a, b, N, TOL)
 i = 1;
 FA = f(a);
 
@@ -7,7 +7,7 @@ while i <= N
     p = a + (b-a)/2;
     disp((b-a)/2)
     FP = f(p);
-    disp(FP)
+    disp(TOL)
     if FP == 0 || (b-a)/2 < TOL % found the root or close enough
         break
     elseif sign(FA) * sign(FP) > 0 % root in other half
@@ -17,6 +17,7 @@ while i <= N
         b = p;
     end
     i = i + 1;
+    res(i) = p;
 end
 
 outputStr = ['Found root ' num2str(p) ' after ' num2str(i) ' iterations.'];
